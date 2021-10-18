768fc1a066aa29acd6b3165335a7d6fd9b170e69
jacobazmi <jacobazmi@gmail.com>
2021-10-18 11:18:23 +0100

    added motorbike test

---
+++ src/test/java/jacob/MotorbikeTest.java
@@ -0,0 +1,15 @@
+package jacob;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MotorbikeTest {
+
+    @Test
+    public void testMotorbikeColourMethod() {
+        Motorbike testMotorbike = new Motorbike("red","Honda","CB500F",12,"BK56EEP");
+        assertEquals("The submarine was not the same colour","red",testMotorbike.getColour());
+    }
+
+}
